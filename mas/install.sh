a301684ec703e5134527810ce945a69d6769a48e
Lucas do Amaral Saboya <lucas.saboya@gmail.com>
2017-02-04 10:04:25 -0200

    Add MAS and AppStore apps.

---
+++ mas/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mas signin lucas.saboya@gmail.com
+mas upgrade
+# Twitter
+mas install 409789998
+# Airmail
+mas install 918858936
+# Telegram
+mas install 747648890
+# Harvest
+mas install 506189836
