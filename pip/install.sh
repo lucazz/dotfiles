7d9eb89fe783ee9c5b968b384ff413accd900b53
Lucas do Amaral Saboya <lucas.saboya@gmail.com>
2018-08-15 19:26:19 -0300

    add pre-commit; add requirements.txt.

--- pip/install.sh
+++ pip/install.sh
@@ -7,36 +7,4 @@ while true;
 	done 2>/dev/null &
 
 sudo easy_install pip
-sudo pip install\
-	ansible==2.2.1.0\
-	boto\
-	boto3\
-	botocore\
-	awscli\
-	s3transfer\
-	futures\
-	jmespath\
-	colorama\
-	docker-py\
-	docker-pycreds\
-	pep8\
-	requests\
-	pylint\
-	pycparser\
-	cffi\
-	six\
-	pyasn1\
-	idna\
-	enum34\
-	ipaddress\
-	cryptography\
-	paramiko\
-	MarkupSafe\
-	jinja2\
-	PyYAML\
-	pycrypto\
-	rsa\
-	python-dateutil\
-	docutils\
-	backports.ssl-match-hostname\
-	websocket-client
+sudo -H pip install -r requirements.txt
