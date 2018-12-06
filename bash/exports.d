1e839b14e8d1e16425f4ce804d1d8f252e28e891
Lucas do Amaral Saboya <lucas@saboya.pro>
2018-12-06 10:05:36 -0300

    Add golang binaires built to PATH env var.

--- bash/exports.d
+++ bash/exports.d
@@ -1,9 +1,10 @@
 #!/bin/bash
+export GOPATH="/Users/Lucas/Projects/Golang"
 export PATH="$PATH:/usr/local/bin/"
 export PATH="/usr/local/opt/curl/bin:$PATH"
 export PATH="$PATH:/usr/local/mysql/bin"
+export PATH="$PATH:$GOPATH/bin"
 export AUTO_MOUNT_HOME=false
-export GOPATH="/Users/Lucas/Projects/Golang"
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_INSECURE_REDIRECT=1
 export PATH="/usr/local/sbin:$PATH"
