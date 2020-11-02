0925c69769549fa67341277b5b07d9e556ccf8b2
Lucas do Amaral Saboya <lucas@saboya.pro>
2020-11-02 16:43:23 -0300

    remove übersicht.

--- ubersicht/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true;
-  do
-    sudo -n true;
-    sleep 60;
-    kill -0 "$$" || exit;
-  done 2>/dev/null &
-
-ln -s ~/.dotfiles/ubersicht/zenbar ~/Library/Application\ Support/Übersicht/widgets/zenbar
