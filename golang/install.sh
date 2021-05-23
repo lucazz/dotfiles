3cccb1b2c58e797c1d01269b846b42539781c000
Lucas do Amaral Saboya <lucas@saboya.pro>
2021-05-23 14:54:13 -0300

    Add golang install & toolling.

--- golang/install.sh
+++ golang/install.sh
@@ -6,5 +6,13 @@ while true;
     kill -0 "$$" || exit;
   done 2>/dev/null &
 
+export GOPATH="${HOME}/.go"
+export GOROOT="$(brew --prefix golang)/libexec"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+test -d "${GOPATH}" || mkdir "${GOPATH}"
+test -d "${GOPATH}/src/github.com" || mkdir -p "${GOPATH}/src/github.com"
+
 go get -u \
-  golang.org/x/tools/gopls
+  golang.org/x/tools/gopls \
+  golang.org/x/tools/cmd/godoc\
+  go get github.com/golang/lint/golint
